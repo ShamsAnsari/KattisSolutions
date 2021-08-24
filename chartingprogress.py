6fc08d2c5fcb56dbe23448c73f17c36df11945ec
Shams Ansari <shamsahmedansari@gmail.com>
2021-08-24 00:09:09 -0700

    desktop update

--- chartingprogress.py
+++ chartingprogress.py
@@ -1,1 +1,21 @@
+def print_chart(chart):
+    for line in chart:
+        print(line)
 
+def sort_chart(chart):
+    order = {}
+    for line in chart
+result = ""
+try:
+    while True:
+        chart = []
+        while True:
+            line = input()
+            if line == "":
+                break
+            chart.append(line)
+        sort_chart(chart)
+        print_chart(chart)
+except EOFError:
+    pass
+print(result[:-1])
