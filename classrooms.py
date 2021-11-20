cafdee7a729666f12d39f3b292c5e20e0a40d282
Shams Ansari <shamsahmedansari@gmail.com>
2021-11-20 14:43:43 -0800

    updated classrooms

--- classrooms.py
+++ classrooms.py
@@ -1,6 +1,6 @@
-from heapq import *
 from sys import stdin
 import bisect
+
 
 def compatible(activity, classroom):
     if classroom == 0:
@@ -8,8 +8,20 @@ def compatible(activity, classroom):
     # Activity must start after last class
     return classroom < activity[1] 
 
+def bubble(i, array):
+
+    while i < len(array) - 1 and array[i + 1] < array[i]:
+        array[i], array[i + 1] = array[i + 1], array[i]
+        i += 1
+    while i > 0 and array[i - 1] > array[i]:
+        array[i], array[i - 1] = array[i - 1], array[i]
+        i -= 1
+    
+
+
 
 fast_input =  lambda : stdin.readline().strip()
+
 num_activities, num_classrooms = map(int, fast_input().split())
 
 classrooms = [0] * num_classrooms
@@ -18,21 +30,18 @@ classrooms = [0] * num_classrooms
 activities = []
 for i in range(num_activities):
     s_i, f_i = map(int, fast_input().split())
-    heappush(activities, (f_i, s_i))
+    activities.append((f_i, s_i))
     
 
-
+activities.sort()
 num_possible_act = 0
-while len(activities) > 0:
-    activity = heappop(activities)
-    i = bisect.bisect_right(classrooms, activity[1]) - 1
-    while i >= 0:
-        if compatible(activity, classrooms[i]):
-            del classrooms[i]
-            j = bisect.bisect_left(classrooms, activity[0]) 
-            classrooms.insert(j, activity[0])
-            num_possible_act += 1
-            break
-        i -= 1
+
+
+for activity in activities:
+    i = bisect.bisect_left(classrooms, activity[1]) - 1
+    if compatible(activity, classrooms[i]):
+        classrooms[i] = activity[0]
+        bubble(i, classrooms)
+        num_possible_act += 1
 
 print(num_possible_act)
